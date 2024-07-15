438025e3e24004f606602c7a2f058833a39d7d78
Victor Freire <victor.desenvolvedor@gmail.com>
2024-07-15 03:14:01 -0400

    Atualizando com meus arquivos

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,15 +1,15 @@
-
-const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
+const acordeonTriggers = document.querySelectorAll(".acordeon .trigger");
 
 acordeonTriggers.forEach((trigger) => {
-    trigger.addEventListener('click', (e) => {
-        const acordeon = trigger.parentElement
-        const isOpen = acordeon.classList.contains('open')
+  trigger.addEventListener("click", (e) => {
+    const acordeon = trigger.parentElement;
 
-        if (isOpen) {
-            acordeon.classList.remove('open')
-        } else {
-            acordeon.classList.add('open')
-        }
-    })
-})
+    const isOpen = acordeon.classList.contains("open");
+
+    if (isOpen) {
+      acordeon.classList.remove("open");
+    } else {
+      acordeon.classList.add("open");
+    }
+  });
+});
--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,6 @@
-
-async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+async function fetchProfileData(params) {
+  const url =
+    "https://raw.githubusercontent.com/victorfpereira/js-developer-portfolio/main/data/profile.json";
+  const fetching = await fetch(url);
+  return await fetching.json();
 }
